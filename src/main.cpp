6124e740d1722c3ae6c2673f9b1e662dab98213b
ydk0208 <ydk208@naver.com>
2022-02-03 21:55:57 +0900

    gyro works!!

--- src/main.cpp
+++ src/main.cpp
@@ -8,6 +8,11 @@
 /*----------------------------------------------------------------------------*/
 
 // ---- START VEXCODE CONFIGURED DEVICES ----
+// Robot Configuration:
+// [Name]               [Type]        [Port(s)]
+// Left                 motor         1               
+// Right                motor         2               
+// gyro1                inertial      10              
 // ---- END VEXCODE CONFIGURED DEVICES ----
 
 #include "vex.h"
@@ -17,5 +22,14 @@ using namespace vex;
 int main() {
   // Initializing Robot Configuration. DO NOT REMOVE!
   vexcodeInit();
-  
+  double pitch,roll,yaw;
+  while(1){
+    roll = gyro1.roll(rotationUnits::deg);
+    pitch = gyro1.pitch(rotationUnits::deg);
+    yaw = gyro1.yaw(rotationUnits::deg);
+    Brain.Screen.setCursor(1,1);
+    Brain.Screen.print(pitch);
+    Right.spin(directionType::rev,(int)pitch,velocityUnits::pct);
+    wait(20,timeUnits::msec);
+  }
 }
--- src/robot-config.cpp
+++ src/robot-config.cpp
@@ -1,15 +1,26 @@
 #include "vex.h"
 
 using namespace vex;
+using signature = vision::signature;
+using code = vision::code;
 
-// A global instance of brain used for printing to the V5 brain screen
-brain Brain;
+// A global instance of brain used for printing to the V5 Brain screen
+brain  Brain;
+
+// VEXcode device constructors
+motor Left = motor(PORT1, ratio18_1, false);
+motor Right = motor(PORT2, ratio18_1, false);
+inertial gyro1 = inertial(PORT11);
+
+// VEXcode generated functions
+
+
 
 /**
  * Used to initialize code/tasks/devices added using tools in VEXcode Pro.
- *
+ * 
  * This should be called at the start of your int main function.
  */
-void vexcodeInit(void) {
-  // Nothing to initialize
+void vexcodeInit( void ) {
+  // nothing to initialize
 }
